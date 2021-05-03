f00ab54b23f2d927202b39ec16adda0c3ef6d00e
unknown <bakewelljake@gmail.com>
2021-05-03 09:42:48 -0500

    Super Ninja

---
+++ superNinja.js
@@ -0,0 +1,54 @@
+class Ninja {
+    constructor(name, health) {
+        this.name = name;
+        this.health = health;
+        this.speed = 3;
+        this.strength = 3;
+    }
+    sayName() {
+        console.log(`I am ${this.name} the ninja`);
+    }
+
+    showStats() {
+        console.log(`Name: ${this.name}`);
+        console.log(`Health: ${this.health}`);
+        console.log(`Speed: ${this.speed}`);
+        console.log(`Strength: ${this.strength}`);
+    }
+
+    drinkSake() {
+        this.health += 10;
+        console.log(`${this.name} drank sake and gained health`);
+    }
+}
+
+const ninja1 = new Ninja("Hyabusa", 5);
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();
+ninja1.showStats();
+
+
+class Sensei extends Ninja {
+    constructor(name) {
+        super(name, 200);
+        this.speed = 10;
+        this.strength = 10;
+        this.wisdom = 10;
+    }
+
+    showStatsSensei() {
+        super.showStats();
+        console.log(`Wisdom: ${this.wisdom}`);
+    }
+
+    speakWisdom() {
+        super.drinkSake();
+        console.log("Live a life of purpose");
+    }
+}
+
+const superSensei = new Sensei("Kakashi");
+superSensei.showStatsSensei();
+superSensei.speakWisdom();
+superSensei.showStatsSensei();
